0f30a894b5ff20b8deecc777f58505d4f4d70ab5
ehdnsj <ina80118@gmail.com>
2020-10-07 19:21:58 +0900

    add beUnhappy function

--- sample.js
+++ sample.js
@@ -1,6 +1,12 @@
+function beunhappy() {
+  console.log("Please be Worry");
+  console.log("Don't be Happy");
+}
+
 function behappy() {
   console.log("Don't Worry");
   console.log("Be Happy}");
 }
 
 behappy();
+beunhappy();
